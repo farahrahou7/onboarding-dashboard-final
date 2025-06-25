75396897f331fe56d9f5452c35db5469e43136eb
farahrahou7 <info.Farahrahou@gmail.com>
2025-06-25 15:29:35 +0200

    Update materials.js

--- frontend/pages/api/materials.js
+++ frontend/pages/api/materials.js
@@ -1,16 +1,1 @@
-import clientPromise from "../../../lib/mongodb";
 
-export default async function handler(req, res) {
-  const client = await clientPromise;
-  const db = client.db("onboarding");
-  const collection = db.collection("materials");
-
-  if (req.method === "GET") {
-    const data = await collection.find({}).toArray();
-    res.json(data);
-  } else if (req.method === "POST") {
-    const material = req.body;
-    await collection.insertOne(material);
-    res.status(201).json({ message: "Material saved" });
-  }
-}

b87b89ea3f73468681b0877387af3c75867c280d
farahrahou7 <info.Farahrahou@gmail.com>
2025-06-25 23:42:29 +0200

    Update activities.js

--- frontend/pages/api/activities.js
+++ frontend/pages/api/activities.js
@@ -1,1 +1,17 @@
+import clientPromise from "../../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("onboarding");
+  const collection = db.collection("activities");
+
+  if (req.method === "GET") {
+    const activities = await collection.find({}).toArray();
+    res.json(activities);Add commentMore actions
+  } else if (req.method === "POST") {
+    const activity = req.body;
+    await collection.insertOne(activity);
+    res.status(201).json({ message: "Activity saved" });
+  }
+}
 

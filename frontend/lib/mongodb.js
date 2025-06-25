5d61113e9cd3b07b09fbe98ab561317c3faa2882
farahrahou7 <info.Farahrahou@gmail.com>
2025-06-25 23:41:59 +0200

    Update mongodb.js

--- frontend/lib/mongodb.js
+++ frontend/lib/mongodb.js
@@ -1,1 +1,25 @@
 
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;Add commentMore actions
+const options = {};
+
+let client;
+let clientPromise;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Voeg je MONGODB_URI toe aan .env bestand");
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
